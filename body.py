0bfdbb0d4806630648260834d4ad2cc3cf4a23da
yadavrahulrao <rahul507538@gmail.com>
2026-01-06 21:36:51 +0530

    body

--- body.py
+++ body.py
@@ -17,6 +17,27 @@
 #     return item
 
 
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+
+
+# app = FastAPI()
+
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return item
+
+
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
