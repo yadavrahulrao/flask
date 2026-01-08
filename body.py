6a3dd6aede2de7cd97d8c7f033d3f743b8caafb0
yadavrahulrao <rahul507538@gmail.com>
2026-01-08 21:12:00 +0530

    body

--- body.py
+++ body.py
@@ -57,6 +57,31 @@
 #     return item
 
 
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+
+
+# app = FastAPI()
+
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     item_dict = item.model_dump()
+#     if item.tax is not None:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"price_with_tax": price_with_tax})
+#     return item_dict
+
+
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -71,10 +96,9 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.model_dump()
-    if item.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
