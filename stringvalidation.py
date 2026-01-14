46a6d3ce10059f92fae7af0dd0cb7f018441385b
yadavrahulrao <rahul507538@gmail.com>
2026-01-14 20:28:58 +0530

    str

--- stringvalidation.py
+++ stringvalidation.py
@@ -43,13 +43,30 @@
 
 
 
+# from fastapi import FastAPI, Query
+
+# app = FastAPI()
+
+
+# @app.get("/items/")
+# async def read_items(q: str | None = Query(default=None, max_length=50)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+from typing import Annotated
+
 from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: str | None = Query(default=None, max_length=50)):
+async def read_items(
+    q: Annotated[str | None, Query(min_length=3, max_length=50)] = None,
+):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
