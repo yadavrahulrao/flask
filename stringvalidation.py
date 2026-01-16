6b8428e2035c2b7571bc13a16ca4e359d9cd5b0b
yadavrahulrao <rahul507538@gmail.com>
2026-01-16 20:41:09 +0530

    anno

--- stringvalidation.py
+++ stringvalidation.py
@@ -75,20 +75,54 @@
 
 
 
+# from typing import Annotated
+
+# from fastapi import FastAPI, Query
+
+# app = FastAPI()
+
+
+# @app.get("/items/")
+# async def read_items(
+#     q: Annotated[
+#         str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
+#     ] = None,
+# ):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+
+
+import random
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
+from pydantic import AfterValidator
 
 app = FastAPI()
+
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
+
+
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
 
 
 @app.get("/items/")
 async def read_items(
-    q: Annotated[
-        str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
-    ] = None,
+    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
 ):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id": id, "name": item}
